14ab7d26d0eb05c694812318764ce2205f9a3224
nguyenkhanh110701 <nhuyenkhanh1107xx@gmail.com>
2024-09-05 08:18:31 +0700

    homepage and admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
+Route::get('/contact', [\App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
+Route::get('/detail', [\App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
+
+
+Route::get('admin/dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.car.index');
