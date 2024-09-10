631f3222b51dac4386fec5bc9c53c866f97d1660
Dustin M_Khoa <tkhoa1122@gmail.com>
2024-09-10 20:07:16 +0700

    login logout generate data ui image pagnigation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;  // Import model Car
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
-        return view('frontend.homepage');
+        // Lấy tất cả xe từ database
+        $cars = Car::paginate(8);
+
+        // Truyền danh sách xe vào view
+        return view('frontend.homepage', compact('cars'))->with('i', (request()->input('page', 1)-1)*10);
     }
 
     public function contact()
@@ -17,8 +36,17 @@ class HomeController extends Controller
         return view('frontend.contact');
     }
 
-    public function detail()
+    public function detail($id)
     {
-        return view('frontend.detail');
+        // Tìm chiếc xe dựa vào ID
+        $car = Car::find($id);
+
+        // Nếu không tìm thấy xe, có thể redirect hoặc hiển thị thông báo lỗi
+        if (!$car) {
+            return redirect()->back()->with('error', 'Car not found.');
+        }
+
+        // Truyền thông tin của xe vào view
+        return view('frontend.detail', compact('car'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
 Route::get('/contact', [\App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
-Route::get('/detail', [\App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
-
+Route::get('/detail/{id}', [\App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
 
 Route::get('admin/dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.car.index');
+
+Auth::routes();
+
